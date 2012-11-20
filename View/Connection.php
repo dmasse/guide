e4ac71f3d3d3b23874c7b120c62e9f40cddd3f70
marie <marie@192.168.24.135>
2012-11-20 15:57:52 +0100

    creation d'un formulaire

--- View/Connection.php
+++ View/Connection.php
@@ -4,7 +4,14 @@
 $mon_form = new Form('POST'); // 'identifiant_unique'
 // Ajout d'un champ texte nommé "prenom" 
 $mon_form->add('Text', 'prenom')         
-->label('Votre prénom SVP');
+->label('Prénom ');
+$mon_form->add('Text', 'nom')
+->label('Nom');
+$mon_form->add('Text', 'adresseMail')
+->label('Adresse Mail');
+$mon_form->add('Text', 'identifiant2')
+->label('Choississez un identifiant');
+$mon_form->add('Submit', 'submit');
 // Affichage du formulaire
  echo $mon_form;
  ?>
