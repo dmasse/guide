4ce1ad79fdbcc7e039158cf8769a97f7a3bf8f46
marie <marie@192.168.24.135>
2012-11-21 14:06:25 +0100

    connection

--- View/Connection.php
+++ View/Connection.php
@@ -1,27 +1,40 @@
 <?php
  include 'Form.php';
 // Création d'un objet Form. // L'identifiant est obligatoire ! 
-$mon_form = new Form('POST'); // 'identifiant_unique'
+$form_inscription = new Form('POST'); // 'identifiant_unique'
 
-$mon_form->add('Radio', 'choixPersonne')  
+$form_inscription->add('Radio', 'choixPersonne')  
  ->label('Je suis ')
  ->choices(array('touriste' => 'un touriste',
  		'guide' => 'un guide', ));
 
 // Ajout d'un champ texte nommé "prenom"
-$mon_form->add('Text', 'prenom')         
+$form_inscription->add('Text', 'prenom')         
 ->label('Prenom ');
 
-$mon_form->add('Text', 'nom')
+$form_inscription->add('Text', 'nom')
 ->label('Nom');
 
-$mon_form->add('Text', 'email')
+//champ adresse mail
+$form_inscription->add('Text', 'email')
 ->label('Adresse Mail');
 
-$mon_form->add('Text', 'identifiant2')
+//champ mot de passe plus verification du mot de passe
+$form_inscription->add('Password', 'mdp')
+->label(" mot de passe ");
+
+$form_inscription->add('Password', 'mdp_verif')                
+ ->label("Confirmation mot de passe ");
+
+
+$form_inscription->add('Text', 'identifiant2')
 ->label('Choississez un identifiant');
 
-$mon_form->add('Submit', 'submit');
+//bouton valider
+$form_inscription->add('Submit', 'submit')
+->value("m'inscrire !");
+
 // Affichage du formulaire
- echo $mon_form;
+ echo $form_inscription;
+ include ('\controller\Connection.php')
  ?>
